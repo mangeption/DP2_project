9a1ee04cac2960f2759ec13f075237c85f26bf54
Kyle Nguyen <ngockhanh5794@gmail.com>
2017-09-25 16:37:18 -0700

    Update current implementation

--- pharmacyDB/routes/route.js
+++ pharmacyDB/routes/route.js
@@ -9,11 +9,12 @@ router.get('/products', function(req, res, next){
     });
 });
 
-router.post('/products', function(req, res, next){
+router.post('/product', function(req, res, next){
     let newProduct = new Products({
         id: req.body.id,
         name: req.body.name,
         price: req.body.price,
+        stock: req.body.stock
     });
     newProduct.save(function(err, prod){
         if(err){
@@ -24,8 +25,43 @@ router.post('/products', function(req, res, next){
     });
 });
 
-router.delete('/products', function(req, res, next){
-    res.send('Retrive products');
+router.put('/editProduct/:id', function(req, res, next){
+    Products.findOne({id: req.params.id}, function(err, foundObject){
+        if(err){
+            res.json(err);
+        }else{
+            if(foundObject){
+                if(req.body.name){
+                    foundObject.name = req.body.name;
+                }
+                if(req.body.price){
+                    foundObject.price = req.body.price;
+                }
+                if(req.body.stock){
+                    foundObject.stock = req.body.stock;
+                }
+                foundObject.save(function(err, updatedObject){
+                    if(updatedObject){
+                        res.json({msg: 'Product editted successfully'})
+                    }else{
+                        res.json({msg: 'Fail to edit document'})
+                    }
+                });
+            }
+        }
+    });
+});
+
+router.delete('/product/:id', function(req, res, next){
+    Products.remove({id: req.params.id}, function(err, result){
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result);
+        }
+    });
+
+    
 });
 
 module.exports = router;
