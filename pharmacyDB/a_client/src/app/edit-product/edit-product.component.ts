70814816322d0829effe576a48feed4184283548
Kyle Nguyen <ngockhanh5794@gmail.com>
2017-10-02 21:43:36 -0700

    Finish edit component

---
+++ pharmacyDB/a_client/src/app/edit-product/edit-product.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductsService} from '../products.service';
+import {Products} from '../products';
+import {Location} from '@angular/common';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-edit-product',
+  templateUrl: './edit-product.component.html',
+  styleUrls: ['./edit-product.component.css'],
+  providers: [ProductsService]
+})
+export class EditProductComponent implements OnInit {
+
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  
+  constructor(private productsService: ProductsService, private location : Location, 
+    private route: ActivatedRoute) { 
+    
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {this.id = +params['id'];});
+  }
+ 
+  editProduct(){
+    const selProduct={
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      stock: this.stock
+    }
+    this.productsService.editProduct(selProduct.id, selProduct).subscribe(product => {
+      this.location.back();
+    });
+  }
+  goBack(){
+    this.location.back();
+  }
+
+}
