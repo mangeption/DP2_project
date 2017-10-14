3c26a1e3b951985b89603b950f6195ba279fd3b0
khanh1202 <dgkhanh97@gmail.com>
2017-10-14 14:12:20 +1100

    Add sale UI

--- pharmacyDB/a_client/src/app/add-sale/add-sale.component.ts
+++ pharmacyDB/a_client/src/app/add-sale/add-sale.component.ts
@@ -2,21 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import {SaleService} from '../sale.service';
 import {Sales} from '../sales';
 import {Location} from '@angular/common';
+import {Products} from '../products';
+import {ProductsService} from '../products.service'
 
 @Component({
   selector: 'app-add-sale',
   templateUrl: './add-sale.component.html',
   styleUrls: ['./add-sale.component.css'],
-  providers: [SaleService]
+  providers: [SaleService, ProductsService]
 })
 export class AddSaleComponent implements OnInit {
   id : number;
   prodId: number;
   date: Date;
   qty: number;
-  constructor(private saleService: SaleService, private location: Location) { }
+  products : Products[];
+  showtheform : boolean;
+  constructor(private saleService: SaleService, private location: Location, private productService: ProductsService) {
+    this.showtheform = false;
+   }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe(products => this.products = products);
   }
   addProduct(){
     const newSale={
@@ -28,6 +35,18 @@ export class AddSaleComponent implements OnInit {
     this.saleService.addSale(newSale).subscribe(() => {
       this.location.back();
     });
+  }
+
+  getShowTheForm()
+  {
+    console.log(this.showtheform);
+    return this.showtheform;
+  }
+
+  reverseShowTheForm()
+  {
+    console.log("Reverse");
+    this.showtheform = ! this.showtheform;
   }
 
 }
